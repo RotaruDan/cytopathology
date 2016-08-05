023e7901953993d493a31f1c61ce2aa5450ecb1a
RotaruDan <drotaru@ucm.es>
2016-08-05 16:28:24 +0200

    DownloadChallenges script now also downloads Courses.

--- desktop/src/es/eucm/cytochallenge/desktop/DownloadChallenges.java
+++ desktop/src/es/eucm/cytochallenge/desktop/DownloadChallenges.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglNet;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.net.HttpRequestBuilder;
 import com.badlogic.gdx.utils.*;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import es.eucm.cytochallenge.model.course.Course;
+import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,17 +17,30 @@ import java.io.OutputStream;
 
 public class DownloadChallenges {
 
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
     public static final String API_PATH = "http://cytopathology.e-ucm.es/";
-    public static final String GET_CHALLENGES_PATH = API_PATH + "challenges";
+    public static final String GET_CHALLENGES_PATH = API_PATH + "api/challenges";
+    public static final String GET_COURSES_PATH = API_PATH + "api/courses";
     public static final String GET_CHALLENGES_UPLOAD_PATH = API_PATH + "uploads/";
     public static final String CHALLENGE_ZIP_NAME = "challenge.zip";
 
+    public static final Array<Course> courses = new Array<Course>();
 
+    private static String authorization;
     public static Array<String> IDS = new Array<String>();
     public static FileHandle TMP_FOLDER, CHALLENGES_FOLDER;
     public static int PROCESSED = 0;
 
     public static void main(String[] arg) {
+
+        String username = System.getenv(USERNAME);
+        String password = System.getenv(PASSWORD);
+        String auth = username + ":" + password;
+        authorization = "Basic " + Base64.encode(auth.getBytes());
+        System.out.println("authorization = " + authorization);
+        System.out.println("auth = " + auth);
+
         Gdx.net = new LwjglNet();
         Gdx.files = new LwjglFiles();
 
@@ -32,7 +48,104 @@ public class DownloadChallenges {
         CHALLENGES_FOLDER = Gdx.files.absolute("./android/assets/challenges");
 
         HttpRequestBuilder requestBuilder = new HttpRequestBuilder();
-        final Net.HttpRequest httpRequest = requestBuilder.newRequest().method(Net.HttpMethods.GET).url(GET_CHALLENGES_PATH).build();
+        final Net.HttpRequest httpRequest = requestBuilder.newRequest().method(Net.HttpMethods.GET).url(GET_COURSES_PATH)
+                .header("Authorization", authorization).build();
+
+        Gdx.net.sendHttpRequest(httpRequest, new Net.HttpResponseListener() {
+            @Override
+            public void handleHttpResponse(Net.HttpResponse httpResponse) {
+                String coursesStr = httpResponse.getResultAsString();
+                System.out.println(coursesStr);
+
+                JsonReader reader = new JsonReader();
+
+                JsonValue value = reader.parse(coursesStr);
+
+                JsonValue courseInfo;
+
+                courses.clear();
+                int i = 0;
+                do {
+                    courseInfo = value.get(i);
+
+                    if (courseInfo != null) {
+
+                        String id = courseInfo.getString("_id");
+
+                        if (id != null) {
+                            System.out.println("courseInfo = " + id);
+
+                            Course course = new Course();
+                            course.setCourseId(id);
+                            course.setName(courseInfo.getString("name"));
+
+                            courses.add(course);
+                        }
+                    }
+                    ++i;
+                } while (courseInfo != null);
+                downloadChallenges();
+            }
+
+            @Override
+            public void failed(Throwable t) {
+                System.out.println("Failed to get courses, t = " + t);
+            }
+
+            @Override
+            public void cancelled() {
+                System.out.println("cancelled");
+            }
+        });
+
+        do {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while ((IDS.size == 0) || IDS.size != PROCESSED);
+
+        Json json = new Json();
+        json.setOutputType(JsonWriter.OutputType.json);
+        FileHandle jsonChallenges = CHALLENGES_FOLDER.child("challenges.json");
+        Array<String> currentChallenges = null;
+        if(jsonChallenges.exists()) {
+            currentChallenges = json.fromJson(Array.class, jsonChallenges);
+        } else {
+            currentChallenges = new Array<String>();
+        }
+
+        for (int i = 0; i < IDS.size; i++) {
+            String id = IDS.get(i);
+            if (id != null && !currentChallenges.contains(id, false)) {
+                currentChallenges.add(id);
+            }
+        }
+        String[] totalChallenges = new String[currentChallenges.size];
+        for (int i = 0; i < currentChallenges.size; i++) {
+            totalChallenges[i] = currentChallenges.get(i);
+        }
+
+        json.toJson(currentChallenges, String[].class, jsonChallenges);
+        System.out.println(json.prettyPrint(currentChallenges));
+
+
+
+        FileHandle coursesFile = CHALLENGES_FOLDER.child("courses.json");
+        json.toJson(courses, Array.class, coursesFile);
+        System.out.println(json.prettyPrint(courses));
+
+        // Cleaning
+        TMP_FOLDER.deleteDirectory();
+    }
+
+    public static void downloadChallenges() {
+
+        HttpRequestBuilder requestBuilder = new HttpRequestBuilder();
+        final Net.HttpRequest httpRequest = requestBuilder.newRequest().method(Net.HttpMethods.GET).url(GET_CHALLENGES_PATH)
+                .header("Authorization", authorization).build();
 
         Gdx.net.sendHttpRequest(httpRequest, new Net.HttpResponseListener() {
             @Override
@@ -57,6 +170,24 @@ public class DownloadChallenges {
                         if (id != null) {
                             System.out.println("challenge = " + id);
                             IDS.add(id);
+
+                            String courseId = challengeInfo.getString("_course");
+
+                            for (int j = 0; j < courses.size; j++) {
+                                Course course = courses.get(j);
+                                if(course.getCourseId().equals(courseId)) {
+                                    Array<String> courseChallenges = course.getChallenges();
+                                    if(courseChallenges == null) {
+                                        courseChallenges = new Array<String>();
+                                        course.setChallenges(courseChallenges);
+                                    }
+
+                                    if(!courseChallenges.contains(id, false)) {
+                                        courseChallenges.add(id);
+                                    }
+                                }
+                            }
+
                             downloadChallenge(id);
                         }
                     }
@@ -66,7 +197,7 @@ public class DownloadChallenges {
 
             @Override
             public void failed(Throwable t) {
-                System.out.println("t = " + t);
+                System.out.println("Failed to get challenges, t = " + t);
             }
 
             @Override
@@ -74,42 +205,14 @@ public class DownloadChallenges {
                 System.out.println("cancelled");
             }
         });
-        do {
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } while ((IDS.size == 0) || IDS.size != PROCESSED);
-
-        Json json = new Json();
-        json.setOutputType(JsonWriter.OutputType.json);
-        FileHandle jsonChallenges = CHALLENGES_FOLDER.child("challenges.json");
-        Array<String> currentChallenges = json.fromJson(Array.class, jsonChallenges);
-
-        for (int i = 0; i < IDS.size; i++) {
-            String id = IDS.get(i);
-            if(id != null && !currentChallenges.contains(id, false)) {
-                currentChallenges.add(id);
-            }
-        }
-        String[] totalChallenges = new String[currentChallenges.size];
-        for (int i = 0; i < currentChallenges.size; i++) {
-            totalChallenges[i] = currentChallenges.get(i);
-        }
-
-        json.toJson(currentChallenges, String[].class, jsonChallenges);
-        System.out.println(json.prettyPrint(currentChallenges));
-
-        // Cleaning
-        TMP_FOLDER.deleteDirectory();
     }
 
     public static void downloadChallenge(final String id) {
         HttpRequestBuilder requestBuilder = new HttpRequestBuilder();
         final Net.HttpRequest httpRequest = requestBuilder.newRequest()
-                .method(Net.HttpMethods.GET).url(GET_CHALLENGES_UPLOAD_PATH + id + "/" + CHALLENGE_ZIP_NAME).build();
+                .method(Net.HttpMethods.GET).url(GET_CHALLENGES_UPLOAD_PATH + id + "/" + CHALLENGE_ZIP_NAME)
+                .header("Authorization", authorization).build();
 
         Gdx.net.sendHttpRequest(httpRequest, new Net.HttpResponseListener() {
             @Override
