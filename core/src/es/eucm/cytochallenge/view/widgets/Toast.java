b5a4c1a18abbe8cd6a53560160c7970daf89f5a9
RotaruDan <drotaru@ucm.es>
2016-08-25 15:23:56 +0200

    Improves toast functionality

--- core/src/es/eucm/cytochallenge/view/widgets/Toast.java
+++ core/src/es/eucm/cytochallenge/view/widgets/Toast.java
@@ -1,8 +1,12 @@
 package es.eucm.cytochallenge.view.widgets;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -23,16 +27,36 @@ public class Toast extends LinearLayout {
     public Toast(ToastStyle style) {
         super(true);
         add(label = new Label("", style.label)).centerY().centerX();
-        setTouchable(Touchable.enabled);
+        setTouchable(Touchable.disabled);
         background(style.background);
         backgroundColor(style.color);
         this.setColor(style.color);
+        pad(WidgetBuilder.dpToPixels(8));
     }
 
     @Override
     protected void drawChildren(Batch batch, float parentAlpha) {
         super.drawChildren(batch, parentAlpha);
         batch.setColor(Color.WHITE);
+    }
+
+    public void show() {
+        pack();
+        float y = 0;
+        setY(-getHeight());
+        setX((Gdx.graphics.getWidth() - getWidth()) * .5f);
+        clearActions();
+        addAction(Actions.sequence(Actions.moveTo(getX(), y, 0.33f, Interpolation.exp5Out), Actions.delay(2f, hide())));
+    }
+
+    private Action hide() {
+        return Actions.sequence(Actions.moveTo(getX(), -getHeight(),
+                0.33f, Interpolation.exp5Out), Actions.run(new Runnable() {
+            @Override
+            public void run() {
+                remove();
+            }
+        }));
     }
 
     public void setText(String text) {
