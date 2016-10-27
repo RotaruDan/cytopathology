e113c0c28c0b46580563391948dc5b339f3d9cc9
RotaruDan <drotaru@ucm.es>
2016-10-27 19:59:00 +0200

    Improves PolygonActor's line width

--- core/src/es/eucm/cytochallenge/view/widgets/challenge/result/PolygonActor.java
+++ core/src/es/eucm/cytochallenge/view/widgets/challenge/result/PolygonActor.java
@@ -9,6 +9,7 @@ import es.eucm.cytochallenge.view.screens.BaseScreen;
 public class PolygonActor extends Actor {
 
     private float[] vertices;
+    private float width = 8f;
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
@@ -16,9 +17,24 @@ public class PolygonActor extends Actor {
 
         BaseScreen.shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
         BaseScreen.shapeRenderer.setTransformMatrix(batch.getTransformMatrix());
-        BaseScreen.shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        BaseScreen.shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         BaseScreen.shapeRenderer.setColor(getColor());
-        BaseScreen.shapeRenderer.polygon(vertices);
+        int count = vertices.length;
+        for (int i = 0, n = count - 2; i < n; i += 2) {
+            float x1 = vertices[i];
+            float y1 = vertices[i + 1];
+
+            float x2;
+            float y2;
+
+            x2 = vertices[i + 2];
+            y2 = vertices[i + 3];
+
+            BaseScreen.shapeRenderer.rectLine(x1, y1, x2, y2, width);
+        }
+        if (count > 5) {
+            BaseScreen.shapeRenderer.rectLine(vertices[0], vertices[1], vertices[count - 2], vertices[count - 1], width);
+        }
         BaseScreen.shapeRenderer.end();
 
         batch.begin();
